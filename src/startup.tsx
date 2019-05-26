c3ee754230478aa9a770321e310c169ca1b27036
Alex Birch <Birch-san@users.noreply.github.com>
2019-05-26 11:54:17 +0100

    some harmonics

--- src/startup.tsx
+++ src/startup.tsx
@@ -4,7 +4,7 @@ const ctx = new AudioContext()
 const synth = new AudioSynth({ctx})
 const voiceProfile = voiceProfiles.piano;
 const factory = synth.makeVoiceFactory(voiceProfile);
-const voice = factory(220);
+// const voice = factory(220);
 // const generator = voice.generate(ctx.currentTime);
 // const arr = new Float32Array(100);
 // for (let i=0; i<100; i++) {
@@ -20,9 +20,75 @@ const voice = factory(220);
 // voice.playFor(ctx, 1)
 // voice.playInFor(ctx, 1, 1)
 // voice.after(1, voice.playFor.bind(voice, ctx, 1))
-voice.fluent()
+factory(220)
+.fluent()
 .playFor(1)
-.playFromFor(1, 1)
+// .playFromFor(1, 1.5)
 .perform(ctx)
+
+factory(440)
+.fluent()
+.playFor(1)
+.perform(ctx)
+
+factory(660)
+.fluent()
+.playFor(1)
+.perform(ctx)
+
+factory(880)
+.fluent()
+.playFor(1)
+.perform(ctx)
+
+factory(1100)
+.fluent()
+.playFor(1)
+.perform(ctx)
+
+factory(1320)
+.fluent()
+.playFor(1)
+.perform(ctx)
+
+factory(1540)
+.fluent()
+.playFor(1)
+.perform(ctx)
+
+factory(1760)
+.fluent()
+.playFor(1)
+.perform(ctx)
+
+factory(1980)
+.fluent()
+.playFor(1)
+.perform(ctx)
+
+// factory(2227.5)
+// .fluent()
+// .playFor(1)
+// .perform(ctx)
+
+// factory(2475)
+// .fluent()
+// .playFor(1)
+// .perform(ctx)
+
+// factory(2722.5)
+// .fluent()
+// .playFor(1)
+// .perform(ctx)
+
+// factory(2970)
+// .fluent()
+// .playFor(1)
+// .perform(ctx)
+
+// factory(3217.5)
+// .fluent()
+// .playFor(1)
+// .perform(ctx)
 
 export const _ = null;
